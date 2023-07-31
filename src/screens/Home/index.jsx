c83baf1b0013d08b57cc12cd3c8dbf41f37d6004
AylenLucero <luceroaylen97@gmail.com>
2023-07-31 02:19:46 -0300

    modificando errores de home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -9,25 +9,25 @@ const Home = () => {
         <div className="container">
             <Nav />
             <div className="body-home">
-               <div className="page">
-                    <H1 txt="AYLEN LUCERO" clas="txt-name-cv" /> 
-                    <div className="lineaCenter">
-                        <Linea clas="linea" />
+                <div className="container-body-home">
+                    <div className="page">
+                        <H1 txt="AYLEN LUCERO" clas="txt-name-cv" /> 
+                        <div className="lineaCenter">
+                            <Linea clas="linea" />
+                        </div>
+                        
+                        <H2 txt="FRONTEND DEVELOPER" clas="txt-cv" />
+                        <div className="txt">
+                            <p>
+                            Soy una persona muy curiosa, amable, tengo paciencia y pienso que es muy positivo seguir aprendiendo día a día un poco más. Poseo buena capacidad de organización, soy sociable, responsable y trabajo bien en equipo. Deseo seguir perfeccionándome a futuro y demostrando mis capacidades.
+                            </p>
+                        </div>                    
                     </div>
-                    
-                    <H2 txt="FRONTEND DEVELOPER" clas="txt-cv" />
-                    <div className="txt">
-                        <p>
-                        Soy una persona muy curiosa, amable, tengo paciencia y pienso que es muy positivo seguir aprendiendo día a día un poco más. Poseo buena capacidad de organización, soy sociable, responsable y trabajo bien en equipo. Deseo seguir perfeccionándome a futuro y demostrando mis capacidades.
-                        </p>
-                    </div>                    
+                    <div className="container-img">
+                        <img src={foto} alt="foto cv" className="img-cv"/>  
+                    </div>
                 </div>
-                <div>
-                   <img src={foto} alt="foto cv" className="img-cv"/>  
-                </div>
-                 
-            </div>
-            
+            </div>            
         </div>
     )
 }
